39a3a30c888540d49593e5275d0301f3eeffd4ed
zhuzhe <zhuzheiswho@sina.com>
2012-04-13 14:00:58 +0800

    dev

--- lib/id2path.rb
+++ lib/id2path.rb
@@ -2,8 +2,8 @@ module Id2path
 
 	def create_path root
 		dir1, dir2, file = self.id.to_s.rjust(9, '0').scan(/\d{3}/)
-		Dir.mkdir(root.join(dir1)) unless File.directory?(root.join(dir1))
-		Dir.mkdir(root.join(dir1, dir2)) unless File.directory?(root.join(dir1, dir2))
+		Dir.mkdir(File.join(root, dir1)) unless File.directory?(File.join(root, dir1))
+		Dir.mkdir(File.join(root, dir1, dir2)) unless File.directory?(File.join(root, dir1, dir2))
 		File.join(root, dir1, dir2, file)
 	end
 
